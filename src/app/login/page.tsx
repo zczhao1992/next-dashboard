3fa4e13747e8f073bb7fdd8d234da71c58ccabe7
zczhao1992 <zczhao1992@163.com>
2024-01-03 20:49:24 +0800

    feat: init

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Login() {
+  return (
+    <div>
+      <Button>Button</Button>
+      <Input type="email" placeholder="Email" />
+    </div>
+  );
+}
