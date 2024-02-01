bbe7a7ccc77207698ed79996994960ba28a33009
zczhao1992 <zczhao1992@163.com>
2024-02-01 15:29:22 +0800

    fix: window报错

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
-
+import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const token = globalThis.localStorage.getItem("token");
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
 
-  if (token) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+    if (token) {
+      redirect("/dashboard");
+    } else {
+      redirect("/login");
+    }
+  }, []);
 }
