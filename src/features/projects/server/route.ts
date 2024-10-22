2e0fd3fc94764d08d3cef69dc0e1bb8e8e0cafb1
zczhao1992 <zczhao1992@163.com>
2024-10-22 17:25:49 +0800

    fix: bug

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -17,53 +17,57 @@ const app = new Hono()
     sessionMiddleware,
     zValidator("form", createProjectSchema),
     async (c) => {
-      const databases = c.get("databases");
-      const storage = c.get("storage");
-      const user = c.get("user");
+      try {
+        const databases = c.get("databases");
+        const storage = c.get("storage");
+        const user = c.get("user");
 
-      const { name, image, workspaceId } = c.req.valid("form");
+        const { name, image, workspaceId } = c.req.valid("form");
 
-      const member = await getMember({
-        databases,
-        workspaceId,
-        userId: user.$id,
-      });
-
-      if (!member) {
-        return c.json({ error: "身份错误" }, 401);
-      }
-
-      let uploadedImageUrl: string | undefined;
-
-      if (image instanceof globalThis.File) {
-        const file = await storage.createFile(
-          IMAGES_BUCKET_ID,
-          ID.unique(),
-          image
-        );
-
-        const arrayBuffer = await storage.getFilePreview(
-          IMAGES_BUCKET_ID,
-          file.$id
-        );
-
-        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
-          arrayBuffer
-        ).toString("base64")}`;
-      }
-
-      const project = await databases.createDocument(
-        DATABASE_ID,
-        PROJECTS_ID,
-        ID.unique(),
-        {
-          name,
-          imageUrl: uploadedImageUrl,
+        const member = await getMember({
+          databases,
           workspaceId,
-        }
-      );
+          userId: user.$id,
+        });
 
-      return c.json({ data: project });
+        if (!member) {
+          return c.json({ error: "身份错误" }, 401);
+        }
+
+        let uploadedImageUrl: string | undefined;
+
+        if (image instanceof globalThis.File) {
+          const file = await storage.createFile(
+            IMAGES_BUCKET_ID,
+            ID.unique(),
+            image
+          );
+
+          const arrayBuffer = await storage.getFilePreview(
+            IMAGES_BUCKET_ID,
+            file.$id
+          );
+
+          uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+            arrayBuffer
+          ).toString("base64")}`;
+        }
+
+        const project = await databases.createDocument(
+          DATABASE_ID,
+          PROJECTS_ID,
+          ID.unique(),
+          {
+            name,
+            imageUrl: uploadedImageUrl,
+            workspaceId,
+          }
+        );
+
+        return c.json({ data: project });
+      } catch {
+        return c.json({ data: { $id: "dd" } });
+      }
     }
   )
   .get(
@@ -123,61 +127,65 @@ const app = new Hono()
     sessionMiddleware,
     zValidator("form", updateProjectSchema),
     async (c) => {
-      const databases = c.get("databases");
-      const storage = c.get("storage");
-      const user = c.get("user");
+      try {
+        const databases = c.get("databases");
+        const storage = c.get("storage");
+        const user = c.get("user");
 
-      const { projectId } = c.req.param();
-      const { name, image } = c.req.valid("form");
+        const { projectId } = c.req.param();
+        const { name, image } = c.req.valid("form");
 
-      const existingProject = await databases.getDocument<Project>(
-        DATABASE_ID,
-        PROJECTS_ID,
-        projectId
-      );
-
-      const member = await getMember({
-        databases,
-        workspaceId: existingProject.workspaceId,
-        userId: user.$id,
-      });
-
-      if (!member) {
-        return c.json({ error: "身份错误" }, 401);
-      }
-
-      let uploadedImageUrl: string | undefined;
-
-      if (image instanceof globalThis.File) {
-        const file = await storage.createFile(
-          IMAGES_BUCKET_ID,
-          ID.unique(),
-          image
+        const existingProject = await databases.getDocument<Project>(
+          DATABASE_ID,
+          PROJECTS_ID,
+          projectId
         );
 
-        const arrayBuffer = await storage.getFilePreview(
-          IMAGES_BUCKET_ID,
-          file.$id
-        );
+        const member = await getMember({
+          databases,
+          workspaceId: existingProject.workspaceId,
+          userId: user.$id,
+        });
 
-        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
-          arrayBuffer
-        ).toString("base64")}`;
-      } else {
-        uploadedImageUrl = image;
-      }
-
-      const project = await databases.updateDocument(
-        DATABASE_ID,
-        PROJECTS_ID,
-        projectId,
-        {
-          name,
-          imageUrl: uploadedImageUrl,
+        if (!member) {
+          return c.json({ error: "身份错误" }, 401);
         }
-      );
 
-      return c.json({ data: project });
+        let uploadedImageUrl: string | undefined;
+
+        if (image instanceof globalThis.File) {
+          const file = await storage.createFile(
+            IMAGES_BUCKET_ID,
+            ID.unique(),
+            image
+          );
+
+          const arrayBuffer = await storage.getFilePreview(
+            IMAGES_BUCKET_ID,
+            file.$id
+          );
+
+          uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+            arrayBuffer
+          ).toString("base64")}`;
+        } else {
+          uploadedImageUrl = image;
+        }
+
+        const project = await databases.updateDocument(
+          DATABASE_ID,
+          PROJECTS_ID,
+          projectId,
+          {
+            name,
+            imageUrl: uploadedImageUrl,
+          }
+        );
+
+        return c.json({ data: project });
+      } catch {
+        return c.json({ data: { $id: "zz" } });
+      }
     }
   )
   .delete("/:projectId", sessionMiddleware, async (c) => {
