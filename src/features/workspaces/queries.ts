e697d0eb72d2d4d64124d97a1e2fa2412044c9f0
zczhao1992 <zczhao1992@163.com>
2024-10-22 15:33:54 +0800

    fix： bug

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -103,24 +103,24 @@ export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   }
 };
 
-interface GetWorkspaceInfoProps {
-  workspaceId: string;
-}
+// interface GetWorkspaceInfoProps {
+//   workspaceId: string;
+// }
 
-export const getWorkspaceInfo = async ({
-  workspaceId,
-}: GetWorkspaceInfoProps) => {
-  try {
-    const { databases } = await createSessionClient();
+// export const getWorkspaceInfo = async ({
+//   workspaceId,
+// }: GetWorkspaceInfoProps) => {
+//   try {
+//     const { databases } = await createSessionClient();
 
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    );
+//     const workspace = await databases.getDocument<Workspace>(
+//       DATABASE_ID,
+//       WORKSPACES_ID,
+//       workspaceId
+//     );
 
-    return { name: workspace.name };
-  } catch {
-    return null;
-  }
-};
+//     return { name: workspace.name };
+//   } catch {
+//     return null;
+//   }
+// };
