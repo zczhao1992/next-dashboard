0b812a2efbfc64a04c14b162fb178d2e46b2cbeb
zczhao1992 <zczhao1992@163.com>
2024-10-22 12:26:58 +0800

    fix:bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // 禁用一些实验性功能的运行时检查
+    esmExternals: "loose",
+
+    // 禁用运行时 JS
+    // runtime: "nodejs",
+  },
+};
 
 module.exports = nextConfig;
